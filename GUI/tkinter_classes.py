27e59a2258cc14c685927075a8dce0b0d4965521
Spaced-out/usr/local/bin/python3 /Users/dfrost/Desktop/code/BrainBoard/GUI/tkinter_classes.py-games <devin.frost4311@gmail.com>
2022-02-24 11:16:24 -0500

    Added Graphics for DataBlock class

--- GUI/tkinter_classes.py
+++ GUI/tkinter_classes.py
@@ -20,22 +20,42 @@ class Sketchpad(Canvas):
         super().__init__(parent, **kwargs)
 
 class DataBlock(Frame):
-	def __init__(self, parent, w = 20,h = 20,d = 20,**kwargs):
+	def __init__(self, parent, x=20,y=20,w = 20,h = 20,d = 20,f = "#00ffff", o = "#00aaaa"):
+		self.x = x
+		self.y = y
 		self.w = w
 		self.h = h
 		self.d = d
-		"""
-		Add attributes during initialization
-		k = kwargs.keys()
-		for i in k:
-			self.__setattr__(i, kwargs.get(i))
-		"""
+		self.o = o
+		self.f = f
+		self.block = None
+		self.parent = parent
+	def __draw__(self):
+		x,y = self.x,self.y
+		w,h,d = self.w, self.h, self.d
+		p = [
+			(x,y),
+			(x+w,y),
+			(x+w,y+h),
+			(x,y+h)
+		]
+		p2 = [
+			p3d(x,y,d),
+			p3d(x+w,y,d),
+			p3d(x+w,y+h,d),
+			p3d(x,y+h,d)
+		]
+		canvas.create_polygon([p[0],p[1],p2[1],p2[0]],fill = self.f, outline = self.o) #Top face
+		canvas.create_polygon([p[0],p2[0],p2[3],p[3]],fill = self.f, outline = self.o)# Left Face
+		canvas.create_rectangle(p2[0],p2[2], fill = self.f, outline=self.o)
+
 
 
 window = Tk() #Create tkinter window element
 window.geometry("500x500+0+0")
-
-t= DataBlock(window, p = 100)
-print(t.p)
+canvas = Canvas(window)
+canvas.pack()
+t= DataBlock(canvas)
+t.__draw__()
 
 window.mainloop()
