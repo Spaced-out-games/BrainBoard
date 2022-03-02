6a6b033683aba010ab3c1aea52703d73a1d953a5
Spaced-out/usr/local/bin/python3 /Users/dfrost/Desktop/code/BrainBoard/GUI/tkinter_classes.py-games <devin.frost4311@gmail.com>
2022-03-02 10:28:42 -0500

    Working drag - drop widgets

--- GUI/tkinter_classes.py
+++ GUI/tkinter_classes.py
@@ -3,23 +3,30 @@ from tkinter import ttk
 from general import avgpts, p3d
 from PIL import Image, ImageTk
 
-class Viewport(Canvas):
-	def __init__(self, parent, **kwargs):
-		super().__init__(parent, **kwargs)
-		self.grid(column=0, row=0, sticky=(N, W, E, S))
-		"""
-		Button Bindings go here
-		format:
-		self.bind("<button-code>", self.method)
-		"""
-	
+def make_draggable(widget):
+    widget.bind("<Button-1>", on_drag_start)
+    widget.bind("<B1-Motion>", on_drag_motion)
 
-if __name__ ==  "__main__":
-	window = Tk()
-	window.columnconfigure(0, weight=1)
-	window.rowconfigure(0, weight=1)
-	window.geometry("500x500")
-	canvas = Viewport(window)
-	window.mainloop()
+def on_drag_start(event):
+    widget = event.widget
+    widget._drag_start_x = event.x
+    widget._drag_start_y = event.y
 
+def on_drag_motion(event):
+    widget = event.widget
+    x = widget.winfo_x() - widget._drag_start_x + event.x
+    y = widget.winfo_y() - widget._drag_start_y + event.y
+    widget.place(x=x, y=y)
 
+main = Tk()
+
+frame = Frame(main, bd=4, bg="grey")
+frame.place(x=10, y=10)
+make_draggable(frame)
+canvas = Canvas(frame)
+canvas.grid(column=0, row=0, sticky=(N, W, E, S))
+canvas.create_rectangle(0,0,20,20)
+
+#image =  ImageTk.PhotoImage(file = "Untitled-1.png")
+#Label(main, image=image).pack()
+main.mainloop()
